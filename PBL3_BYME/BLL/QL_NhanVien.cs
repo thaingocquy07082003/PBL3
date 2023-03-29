8bf235d3d390d058d41b43f5edf35116f1f4b119
thai ngoc quy <thaingocquy2003@gmail.com>
2023-03-30 01:27:26 +0700

    add BLL. QL_NhanVien

---
+++ PBL3_BYME/BLL/QL_NhanVien.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3_BYME.BLL
+{
+    public  class QL_NhanVien
+    {
+    }
+}
